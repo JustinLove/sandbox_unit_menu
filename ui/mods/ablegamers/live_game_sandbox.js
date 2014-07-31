2c86d54ccfd47e18933e80d0b7e983e001022a7e
Justin Love <git@JustinLove.name>
2014-07-31 08:00:19 -0500

    display initial queue

--- ui/mods/ablegamers/live_game_sandbox.js
+++ ui/mods/ablegamers/live_game_sandbox.js
@@ -4,18 +4,30 @@
   paths.text = paths.text || 'coui://ui/mods/ablegamers/text'
 
   model.menu_items = ko.observable([])
+  model.sandbox_unit_queue = ko.observable([])
   model.sandbox_copy_menu = function(item) {
     engine.call('unit.debug.setSpecId', item.build[0][1])
+    model.sandbox_unit_queue(item.build)
     api.Panel.message(api.Panel.parentId, 'sandboxMenuItem', item)
+  }
+
+  model.sandbox_unit_queue_item = function(item) {
+    return item.join(' : ')
   }
 })()
 
-require(['ablegamers/menu', 'text!ablegamers/sandbox_menu.html'], function(menu, html) {
+require([
+  'ablegamers/menu',
+  'text!ablegamers/sandbox_menu.html',
+  'text!ablegamers/sandbox_queue.html'
+], function(menu, menuHtml, queueHtml) {
   model.menu_items(menu)
 
-  var $html = $(html)
+  var $menu = $(menuHtml)
+  ko.applyBindings(model, $menu[0])
+  $('.div_sandbox_dock:first').append($menu)
 
-  ko.applyBindings(model, $html[0])
-
-  $('.div_sandbox_dock:first').append($html)
+  var $queue = $(queueHtml)
+  ko.applyBindings(model, $queue[0])
+  $('.div_sandbox_bottom_spacer').append($queue)
 })
