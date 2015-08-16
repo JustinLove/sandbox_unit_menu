4777c5949556c591c7c87b58d3213eed71044b32
Justin Love <git@JustinLove.name>
2015-08-16 14:03:45 -0500

    update keybindings

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Sandbox Unit Menu Changelog
+
+## 0.3.0
+
+Update to current build
+
+- Keybinding interface changed
--- ui/mods/sandbox_unit_menu/keybinding.js
+++ ui/mods/sandbox_unit_menu/keybinding.js
@@ -1,4 +1,11 @@
-action_sets.hacks['paste queue'] = function () {
+action_sets.hacks.paste_queue = function () {
   if (model.pasteShift) model.pasteShift()
 }
-default_keybinds.hacks['paste queue'] = 'alt+v'
+api.settings.definitions.keyboard.settings.paste_queue = {
+  title: 'paste queue',
+  type: 'keybind',
+  set: 'mods',
+  display_group: 'mods',
+  display_sub_group: 'sandbox unit menu',
+  default: 'alt+v'
+}
