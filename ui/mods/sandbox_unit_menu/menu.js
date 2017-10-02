74e72a4c373e24b63b4a06372ccef0a78c55836d
Justin Love <git@JustinLove.name>
2017-10-02 16:08:15 -0500

    add discounts field to menu

--- ui/mods/sandbox_unit_menu/menu.js
+++ ui/mods/sandbox_unit_menu/menu.js
@@ -1,9 +1,9 @@
 define([], function() {
   return [
-    { donation: 1, code: 'A1', build: [
+    { code: 'A1', donation: 1, discounts: [1], build: [
       [8, "/pa/units/land/bot_bomb/bot_bomb.json"],
     ]},
-    { donation: 1, code: 'A2', build: [
+    { code: 'A2', donation: 1, discounts: [1], build: [
       [1, "/pa/units/land/land_mine/land_mine.json"],
       [1, "/pa/units/land/land_mine/land_mine.json"],
       [1, "/pa/units/land/land_mine/land_mine.json"],
@@ -25,93 +25,93 @@ define([], function() {
       [1, "/pa/units/land/land_mine/land_mine.json"],
       [1, "/pa/units/land/land_mine/land_mine.json"],
     ]},
-    { donation: 1, code: 'A3', build: [
+    { code: 'A3', donation: 1, discounts: [1], build: [
       [3, "/pa/units/air/solar_drone/solar_drone.json"],
     ]},
-    { donation: 1, code: 'A4', build: [
+    { code: 'A4', donation: 1, discounts: [1], build: [
       [1, "/pa/units/orbital/orbital_probe/orbital_probe.json"],
     ]},
-    { donation: 1, code: 'A5', build: [
+    { code: 'A5', donation: 1, discounts: [1], build: [
       [5, "/pa/units/air/transport/transport.json"],
     ]},
-    { donation: 1, code: 'A6B', build: [
+    { code: 'A6B', donation: 1, discounts: [1], build: [
       [1, "/pa/units/land/bot_factory/bot_factory.json"],
     ]},
-    { donation: 1, code: 'A6V', build: [
+    { code: 'A6V', donation: 1, discounts: [1], build: [
       [1, "/pa/units/land/vehicle_factory/vehicle_factory.json"],
     ]},
-    { donation: 1, code: 'A6A', build: [
+    { code: 'A6A', donation: 1, discounts: [1], build: [
       [1, "/pa/units/air/air_factory/air_factory.json"],
     ]},
-    { donation: 3, code: 'B1', build: [
+    { code: 'B1', donation: 3, discounts: [3,1], build: [
       [1, "/pa/units/land/tank_heavy_armor/tank_heavy_armor.json"],
     ]},
-    { donation: 3, code: 'B2', build: [
+    { code: 'B2', donation: 3, discounts: [3,1], build: [
       [1, "/pa/units/orbital/orbital_launcher/orbital_launcher.json"],
     ]},
-    { donation: 3, code: 'B3', build: [
+    { code: 'B3', donation: 3, discounts: [3,1], build: [
       [5, "/pa/units/land/tank_armor/tank_armor.json"],
     ]},
-    { donation: 3, code: 'B4', build: [
+    { code: 'B4', donation: 3, discounts: [3,1], build: [
       [1, "/pa/units/orbital/ion_defense/ion_defense.json"],
     ]},
-    { donation: 5, code: 'C1', build: [
+    { code: 'C1', donation: 5, discounts: [5,3,1], build: [
       [1, "/pa/units/land/artillery_unit_launcher/artillery_unit_launcher.json"],
       [1, "/pa/units/land/artillery_unit_launcher/artillery_unit_launcher.json"],
     ]},
-    { donation: 5, code: 'C2', build: [
+    { code: 'C2', donation: 5, discounts: [5,3,1], build: [
       [1, "/pa/units/land/teleporter/teleporter.json"],
       [1, "/pa/units/land/teleporter/teleporter.json"],
     ]},
-    { donation: 5, code: 'C3B', build: [
+    { code: 'C3B', donation: 5, discounts: [5,3,1], build: [
       [1, "/pa/units/land/bot_factory_adv/bot_factory_adv.json"],
     ]},
-    { donation: 5, code: 'C3V', build: [
+    { code: 'C3V', donation: 5, discounts: [5,3,1], build: [
       [1, "/pa/units/land/vehicle_factory_adv/vehicle_factory_adv.json"],
     ]},
-    { donation: 5, code: 'C3A', build: [
+    { code: 'C3A', donation: 5, discounts: [5,3,1], build: [
       [1, "/pa/units/air/air_factory_adv/air_factory_adv.json"],
     ]},
-    { donation: 10, code: 'D1', build: [
+    { code: 'D1', donation: 10, discounts: [10,5,3,1], build: [
       [1, "/pa/units/orbital/orbital_battleship/orbital_battleship.json"],
     ]},
-    { donation: 10, code: 'D2', build: [
+    { code: 'D2', donation: 10, discounts: [10,5,3,1], build: [
       [10, "/pa/units/land/bot_nanoswarm/bot_nanoswarm.json"],
     ]},
-    { donation: 10, code: 'D3', build: [
+    { code: 'D3', donation: 10, discounts: [10,5,3,1], build: [
       [2, "/pa/units/sea/hover_ship/hover_ship.json"],
     ]},
-    { donation: 15, code: 'E1', build: [
+    { code: 'E1', donation: 15, discounts: [15,10,5,3,1], build: [
       [1, "/pa/units/land/bot_support_commander/bot_support_commander.json"],
     ]},
-    { donation: 15, code: 'E2', build: [
+    { code: 'E2', donation: 15, discounts: [15,10,5,3,1], build: [
       [1, "/pa/units/land/tank_nuke/tank_nuke.json"],
     ]},
-    { donation: 15, code: 'E3', build: [
+    { code: 'E3', donation: 15, discounts: [15,10,5,3,1], build: [
       [15, "/pa/units/air/fighter_adv/fighter_adv.json"],
     ]},
-    { donation: 20, code: 'F1', build: [
+    { code: 'F1', donation: 20, discounts: [20,15,10,5,3,1], build: [
       [1, "/pa/units/land/nuke_launcher/nuke_launcher.json"],
     ]},
-    { donation: 10, code: 'G1', build: [
+    { code: 'G1', donation: 10, discounts: [10,5,3,1], build: [
       [1, "/pa/units/orbital/delta_v_engine/delta_v_engine.json"],
     ]},
-    { donation: 20, code: 'G2', build: [
+    { code: 'G2', donation: 20, discounts: [20,15,10,5,3,1], build: [
       [1, "/pa/units/land/control_module/control_module.json"],
     ]},
-    { donation: 25, code: 'G3', build: [
+    { code: 'G3', donation: 25, discounts: [25,20,15,10,5,3,1], build: [
       [1, "/pa/units/land/titan_bot/titan_bot.json"],
     ]},
-    { donation: 25, code: 'G4', build: [
+    { code: 'G4', donation: 25, discounts: [25,20,15,10,5,3,1], build: [
       [1, "/pa/units/air/titan_air/titan_air.json"],
     ]},
-    { donation: 25, code: 'G5', build: [
+    { code: 'G5', donation: 25, discounts: [25,20,15,10,5,3,1], build: [
       [1, "/pa/units/land/titan_vehicle/titan_vehicle.json"],
     ]},
-    { donation: 25, code: 'G6', build: [
+    { code: 'G6', donation: 25, discounts: [25,20,15,10,5,3,1], build: [
       [1, "/pa/units/orbital/titan_orbital/titan_orbital.json"],
     ]},
-    { donation: 40, code: 'G7', build: [
+    { code: 'G7', donation: 40, discounts: [40,25,20,15,10,5,3,1], build: [
       [1, "/pa/units/land/titan_structure/titan_structure.json"],
     ]},
   ]
